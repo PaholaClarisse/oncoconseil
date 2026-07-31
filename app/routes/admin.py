2f751333ceef3a7cdb0185d0fd371b755a732075
PaholaClarisse <paholamartinelagos@gmail.com>
2026-07-31 19:53:52 +0200

    mise a jour des fichier admin.py et chat_service.py

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -48,29 +48,6 @@ def upload_document(file: UploadFile = File(...), db: Session = Depends(get_db))
         "document_id": new_document.id,
         "nombre_chunks": len(chunks)
     }
-""" def upload_document(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    temp_path = f"/tmp/{file.filename}"
-    with open(temp_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    texte = extract_text_from_pdf(temp_path)
-    chunks = split_into_chunks(texte)
-
-    new_document = Document(name=file.filename, nombre_chunks=len(chunks))
-    db.add(new_document)
-    db.commit()
-    db.refresh(new_document)
-
-    for i, chunk in enumerate(chunks):
-        embedding = generate_embedding(chunk)
-        collection.add(
-            ids=[f"{new_document.id}_{i}"],
-            embeddings=[embedding],
-            documents=[chunk],
-            metadatas=[{"document_id": new_document.id}]
-        )
-
-    return {"message": "Document indexé avec succès", "document_id": new_document.id, "nombre_chunks": len(chunks)}"""
 
 @router.get("/documents", response_model=list[DocumentOut])
 def list_documents(
